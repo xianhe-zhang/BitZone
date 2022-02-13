aa21f1f139f79fa36f50a92da664db0dfadac253
xianhezhang <3600276843@qq.com>
2022-02-13 14:35:53 -0800

    env added

--- backend/app.js
+++ backend/app.js
@@ -49,7 +49,7 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    `mongodb+srv://mern:mern@cluster0.aqqnl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.aqqnl.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
   )
   .then(() => {
     app.listen(5000);
